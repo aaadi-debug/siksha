6727912747ef0e74d0239e2f2abb477a6f89ac4d
Aditya Raj Gupta <adityaraj51202@gmail.com>
2025-02-17 04:18:06 +0530

    aditya pc

--- app/components/studentDashboard/AppliedColleges.jsx
+++ app/components/studentDashboard/AppliedColleges.jsx
@@ -1,13 +1,167 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import Breadcrumbs2 from "../../components/Breadcrumbs2";
+import collegeDataJson from "../../data/collegeData.json";
+
+import {
+  MapPin,
+  School,
+  Calendar,
+  GraduationCap,
+  ShieldCheck,
+} from "lucide-react";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import DynamicThemeButton from "../../components/DynamicThemeButton";
+import StarRating from "../../components/StarRating";
 
 const AppliedColleges = () => {
+    const colleges = collegeDataJson.data;
+   // State to control the number of displayed colleges
+    const [visibleCount, setVisibleCount] = useState(30);
+  
+    // Function to load more colleges
+    const loadMoreColleges = () => {
+      setVisibleCount((prevCount) => prevCount + 30);
+    };
+  
+    const createSlug = (id, name, city) => {
+      const cleanedName = encodeURIComponent(
+        name
+          .replace(/[\[\]]/g, "") // Remove square brackets
+          .replace(/[-]+/g, "-") // Replace multiple hyphens with a single hyphen
+          .replace(/\s+/g, "-") // Replace spaces with hyphens
+          .toLowerCase()
+      );
+  
+      const formattedCity = encodeURIComponent(
+        city.toLowerCase().replace(/\s+/g, "-")
+      );
+      return `${id}-${cleanedName}-${formattedCity}`;
+    };
+  
+    // Function to calculate rating stars for individual college
+    const calculateStars = (rating) => {
+      const validRating = isNaN(rating) ? 0 : Math.max(0, Math.min(5, rating)); // Ensure between 0-5
+      const fullStars = Math.floor(validRating);
+      const halfStars = validRating % 1 >= 0.5 ? 1 : 0;
+      const emptyStars = 5 - fullStars - halfStars;
+  
+      return { fullStars, halfStars, emptyStars };
+    };
+
+
   return (
     <>
       <div className="text-2xl font-semibold text-tertiary">
         Applied Colleges
       </div>
-    </>
-  )
-}
+      <div className="grid gap-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 mt-4">
+        {colleges.slice(0, visibleCount).map((college) => {
+          // Calculate reviews for each college individually
+          const reviews = college?.reviews ?? [];
+          const validRatings = reviews
+            .map((review) => parseFloat(review?.studentRating))
+            .filter((rating) => !isNaN(rating)); // Remove invalid ratings
 
-export default AppliedColleges
+          const averageRating =
+            validRatings.length > 0
+              ? validRatings.reduce((sum, rating) => sum + rating, 0) /
+                validRatings.length
+              : 0;
+
+          const { fullStars, halfStars, emptyStars } =
+            calculateStars(averageRating);
+
+          return (
+            <a
+              key={college.id}
+              href={`/college/${createSlug(
+                college.collegeId,
+                college.collegeName,
+                college.collegeAddress.city
+              )}`}
+              className="p-2 border text-tertiary border-gray-300 rounded-lg hover:bg-prim-light transition duration-300"
+            >
+              <div className="flex gap-2">
+                <img
+                  src={
+                    college?.collegeLogo || "/assets/testimonial_noImage.png"
+                  }
+                  alt="College Logo"
+                  className="w-20 h-20 rounded bg-white p-2"
+                />
+                {/* College details */}
+                <div className="">
+                  <h1 className="2xl:text-4xl xl:text-3xl lg:text-2xl max-sm:text-2xl font-semibold pb-2 max-sm:mb-2 text-prim">
+                    {college?.collegeName}
+                  </h1>
+                  {/* Rating Section */}
+                  <div className="flex items-center flex-wrap gap-2 mb-3 mt-2">
+                    <span className="text-tertiary font-bold text-lg">
+                      {averageRating.toFixed(1)}
+                    </span>
+                    {/* Render rating stars */}
+                    <div className="flex items-center text-prim">
+                      <StarRating rating={averageRating} />
+                    </div>
+                    {reviews.length > 0 && (
+                      <span className="text-sm underline text-textClr">
+                        ({reviews.length}{" "}
+                        {reviews.length > 1 ? "Reviews" : "Review"})
+                      </span>
+                    )}
+                  </div>
+
+                  <div>
+                    <div className="flex flex-col items-start">
+                      {(college?.collegeAddress.city ||
+                        college?.collegeAddress.state) && (
+                        <div className="flex items-start text-textClr gap-1 capitalize">
+                          <MapPin size={16} className="text-prim mt-1" />
+                          {college?.collegeAddress.city}
+                          {college?.collegeAddress.city && <>, </>}
+                          {college?.collegeAddress.state}
+                        </div>
+                      )}
+
+                      {college?.collegeType && (
+                        <div className="flex items-center text-textClr gap-1 capitalize">
+                          <School size={16} className="text-prim" />
+                          {college?.collegeType}
+                        </div>
+                      )}
+
+                      {college?.EstdYear && (
+                        <div className="flex items-center text-textClr gap-1">
+                          <Calendar size={16} className="text-prim" />
+                          {college?.EstdYear}
+                        </div>
+                      )}
+
+                      {college?.NAACGrade && (
+                        <div className="flex items-center text-textClr gap-1 capitalize">
+                          <GraduationCap size={16} className="text-prim" />
+                          NAAC Grade {college?.NAACGrade}
+                        </div>
+                      )}
+                    </div>
+                    {college?.approvedBy?.length === 0 ? (
+                      <></>
+                    ) : (
+                      <div className="flex items-center text-textClr gap-1">
+                        <ShieldCheck size={16} className="text-prim" />
+                        {college?.approvedBy.join(", ")}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default AppliedColleges;
--- app/components/studentDashboard/Reviews.jsx
+++ app/components/studentDashboard/Reviews.jsx
@@ -1,9 +1,177 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { ThumbsUp, ThumbsDown } from "lucide-react";
+import StarRating from "../StarRating";
+
+const review = [
+  {
+    _id: 1,
+    studentName: "Aditya Raj Gupta",
+    studentImage: "",
+    studentEmail: "webdegredg@gmail.com",
+    studentPhone: "+91999999999",
+    studentGender: "Male",
+
+    studentRating: "5",
+    universityName: "Sharda University",
+    courseName: "Engineering",
+    yearOfAdmission: "2024",
+    isHelpful: true,
+    date: "Feb 11, 2025",
+    likePoints: ["like 1", "like 2"],
+    dislikePoints: ["dis-like 1", "dis-like 2"],
+    reason2: "Step 2 data",
+    reason4: "Step 4 data",
+    reason5: "Step 5 data",
+    reason6: "Step 6 data",
+    reason7: "Step 7 data",
+  },
+  
+];
 
 const Reviews = () => {
+  const [expandedReviews, setExpandedReviews] = useState({});
+
+  const toggleReadMore = (index) => {
+    setExpandedReviews((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
   return (
     <>
       <div className="text-2xl font-semibold text-tertiary">Reviews</div>
+
+      <div className="mt-4 bg-white rounded-lg ">
+        <div className="font-semibold mb-3">Total reviews: {review.length}</div>
+        <div className="space-y-6">
+          {review.map((review, index) => (
+            <div
+              key={review._id}
+              className="p-6 border border-gray-200 rounded-lg"
+            >
+              <div className="flex items-start justify-between space-x-4">
+                <div className="">
+                  <p className="text-tertiary font-semibold text-lg">
+                    College: {review.universityName} - {review.courseName}
+                  </p>
+                  <span className="text-sm text-gray-500">
+                    Review Date: {review.date}
+                  </span>
+                </div>
+                <div>
+                  <div className="text-yellow-500">
+                    <div className="flex items-center text-prim">
+                      <StarRating rating={review.studentRating} />
+                    </div>
+                  </div>
+                  {review.isHelpful && (
+                    <div
+                      className="px-3 py-1 text-white rounded text-sm text-center bg-green-500 mt-1"
+                      disabled
+                    >
+                      Helpful
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="mt-4 flex justify-between items-center"></div>
+
+              <div className="grid gap-4 grid-cols-2 max-sm:grid-cols-1 mt-2">
+                <div className="bg-green-100 rounded-lg p-2">
+                  <div className="text-green-600 flex gap-2 items-center text-lg font-semibold ">
+                    <ThumbsUp size={20} />
+                    Likes:
+                  </div>
+                  <ul className="list-disc pl-6 mt-2  text-black">
+                    {review.likePoints.map((point, i) => (
+                      <li key={i}>{point}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className="bg-red-100 rounded-lg p-2">
+                  <div className="text-red-600 flex gap-2 items-center text-lg font-semibold ">
+                    <ThumbsDown size={20} />
+                    Dislikes:
+                  </div>
+                  <ul className="list-disc pl-6  mt-2  text-black">
+                    {review.dislikePoints.map((point, i) => (
+                      <li key={i}>{point}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <h4 className="font-semibold">Additional Feedback:</h4>
+                {expandedReviews[index] && (
+                  <div className=" border-t-2 border-dashed mt-4 ">
+                    {review.reason5 && (
+                      <div>
+                        <div className="font-semibold text-md text-tertiary mt-4">
+                          Course Curriculum
+                        </div>
+                        <div className="mt-2 text-textClr">
+                          {review.reason5}
+                        </div>
+                      </div>
+                    )}
+                    {review.reason2 && (
+                      <div>
+                        <div className="font-semibold text-md text-tertiary mt-4">
+                          Fees and Financial Aid
+                        </div>
+                        <div className="mt-2 text-textClr">
+                          {review.reason2}
+                        </div>
+                      </div>
+                    )}
+                    {review.reason4 && (
+                      <div>
+                        <div className="font-semibold text-md text-tertiary mt-4">
+                          Course Structure
+                        </div>
+                        <div className="mt-2 text-textClr">
+                          {review.reason4}
+                        </div>
+                      </div>
+                    )}
+
+                    {review.reason6 && (
+                      <div>
+                        <div className="font-semibold text-md text-tertiary mt-4">
+                          Campus Life
+                        </div>
+                        <div className="mt-2 text-textClr">
+                          {review.reason6}
+                        </div>
+                      </div>
+                    )}
+                    {review.reason7 && (
+                      <div>
+                        <div className="font-semibold text-md text-tertiary mt-4">
+                          Night Life
+                        </div>
+                        <div className="mt-2 text-textClr">
+                          {review.reason7}
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+                <button
+                  className="mt-2 text-second underline hover:text-prim transition duration-300"
+                  onClick={() => toggleReadMore(index)}
+                >
+                  {expandedReviews[index] ? "Read Less" : "Read More"}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
